177c152aaf2eaa626c36b17c88e8e321ad66fc78
Mr. Green <tom-300@hotmail.com>
2020-12-02 23:04:07 -0300

    codigos para mux agregado. revisar

--- tp3-bip/tp3-bip.srcs/sources_1/new/op_decoder.v
+++ tp3-bip/tp3-bip.srcs/sources_1/new/op_decoder.v
@@ -13,7 +13,8 @@ module op_decoder
     output reg        o_write_ram,
     output reg        o_read_ram
 );
-
+	
+	//Operaciones
     localparam [4:0] HLT  = 4'b00000;
     localparam [4:0] STO  = 4'b00001;
     localparam [4:0] LD   = 4'b00010;
@@ -22,6 +23,11 @@ module op_decoder
     localparam [4:0] ADDI = 4'b00101;
     localparam [4:0] SUB  = 4'b00110;
     localparam [4:0] SUBI = 4'b00111;
+	
+	//multiplexores
+	localparam [1:0] MEMORIA   = 2'b00;//ver si no genera 
+	localparam [1:0] OPERANDO  = 2'b01;//problemas a la 
+	localparam [1:0] RESULTADO = 2'b10;//hora de sintesis
 
     always@(*)begin:decode
         if(i_reset)
@@ -30,8 +36,9 @@ module op_decoder
             o_write_pc = 1'b1;
             
         //valores iniciales
-        o_sel_a     = 2'b0;
-        o_sel_b     = 1'b0;
+        o_sel_a     = MEMORIA;
+        o_sel_b     = MEMORIA;
+		
         o_write_acc = 1'b0;
         o_operacion = 1'b0;
         o_write_ram = 1'b0;
@@ -40,54 +47,54 @@ module op_decoder
         case(i_opcode)
             HLT:
             begin
-                o_write_pc = 1'b0;
+                o_write_pc = 1'b0; 
             end
             
-            STO:
+            STO: //escribir en memoria
             begin
                 o_write_ram = 1'b1;
             end
             
-            LD: 
+            LD: //cargar desde memoria
             begin
                 o_write_acc = 1'b1;
                 o_read_ram  = 1'b1;
             end
             
-            LDI:
+            LDI: //cargar literal
             begin
-                o_sel_a     = 2'b01;
+                o_sel_a     = OPERANDO;
                 o_write_acc = 1'b1;
             end
             
-            ADD:
+            ADD: //sumar desde memoria
             begin
-                o_sel_a     = 2'b10;
-                o_sel_b     = 1'b0;
+                o_sel_a     = RESULTADO;
+                o_sel_b     = MEMORIA;
                 o_write_acc = 1'b1;
                 o_read_ram  = 1'b1;
             end
             
-            ADDI:
+            ADDI: //sumar literal
             begin
-                o_sel_a     = 2'b10;
-                o_sel_b     = 1'b1;
+                o_sel_a     = RESULTADO;
+                o_sel_b     = OPERANDO;
                 o_write_acc = 1'b1;
             end
             
-            SUB:
+            SUB: //restar desde memoria
             begin
-                o_sel_a     = 2'b10;
-                o_sel_b     = 1'b0;
+                o_sel_a     = RESULTADO;
+                o_sel_b     = MEMORIA;
                 o_operacion = 1'b1;
                 o_write_acc = 1'b1;
                 o_read_ram  = 1'b1;
             end
             
-            SUBI:
+            SUBI: //restar literal
             begin
-                o_sel_a     = 2'b10;
-                o_sel_b     = 1'b1;
+                o_sel_a     = RESULTADO;
+                o_sel_b     = OPERANDO;
                 o_operacion = 1'b1;
                 o_write_acc = 1'b1;
             end
