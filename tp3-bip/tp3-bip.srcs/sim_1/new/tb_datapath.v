cdd1eee7cda9615e245814c4b582713a53dd262f
Mr. Green <tom-300@hotmail.com>
2020-12-05 21:28:04 -0300

    mejorado con localparams

--- tp3-bip/tp3-bip.srcs/sim_1/new/tb_datapath.v
+++ tp3-bip/tp3-bip.srcs/sim_1/new/tb_datapath.v
@@ -16,6 +16,11 @@ module tb_datapath();
     wire [15:0] o_memory_data;
     wire        clk_out, locked;
     
+    //multiplexores
+	localparam [1:0] MEMORIA   = 2'b00;//ver si no genera 
+	localparam [1:0] OPERANDO  = 2'b01;//problemas a la 
+	localparam [1:0] RESULTADO = 2'b10;//hora de sintesis
+    
     initial begin
         clk           = 1'b0;
         sel_a         = 2'b00;
@@ -33,54 +38,55 @@ module tb_datapath();
 
         //LDI: ACC<-operando (acc = 1)
         #20
-        sel_a         = 2'b01;
-        sel_b         = 1'b0;
+        sel_a         = OPERANDO;
+        sel_b         = MEMORIA;
         write_acc     = 1'b1;
         operation     = 1'b1;
-        operando      = 16'b1;
-        i_memory_data = 16'b10;
+        operando      = 11'b1;
+        i_memory_data = 16'b0; //si pongo >0 las direcciones se 
+                               //hacen negativas (fffe)
         
         //ADD: ACC<-ACC+DRAM[operando] (acc+=4 -> 5)
         #20
-        sel_a         = 2'b10;
-        sel_b         = 1'b0;
+        sel_a         = RESULTADO;
+        sel_b         = MEMORIA;
         operation     = 1'b0;
         write_acc     = 1'b1;
-        operando      = 16'b11;
+        operando      = 11'b11;
         i_memory_data = 16'b100;
         
         //ADDI: ACC<-ACC+operando (acc+=5 -> 10)
         #20
-        sel_a         = 2'b10;
-        sel_b         = 1'b1;
+        sel_a         = RESULTADO;
+        sel_b         = OPERANDO;
         operation     = 1'b0;
         write_acc     = 1'b1;
-        operando      = 16'b101;
+        operando      = 11'b101;
         i_memory_data = 16'b110;
         
         //SUB: ACC<-ACC-DRAM[operando] (acc-=8 -> 2)
         #20
-        sel_a         = 2'b10;
-        sel_b         = 1'b0;
+        sel_a         = RESULTADO;
+        sel_b         = MEMORIA;
         operation     = 1'b1;
         write_acc     = 1'b1;
-        operando      = 16'b111;
+        operando      = 11'b111;
         i_memory_data = 16'b1000;
         
         //SUBI: ACC<-ACC-operando (acc-=1 -> 1)
         #20
-        sel_a         = 2'b00;
-        sel_b         = 1'b1;
+        sel_a         = MEMORIA;
+        sel_b         = OPERANDO;
         operation     = 1'b1;
         write_acc     = 1'b1;
-        operando      = 16'b0001;
+        operando      = 11'b0001;
         i_memory_data = 16'b0001;
         
         #100 
         $finish;
     end
     
-    always #5 clk = ~clk;  // Simulacion de clock
+    always #5 clk = ~clk;  // Simulacion de clock 100MHz
     
      clock u_clock
      (
