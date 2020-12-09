c3ba5dc62b4fc2ce9d6cb288d748fd868c50a479
Mr. Green <tom-300@hotmail.com>
2020-12-09 18:55:54 -0300

    'i_valid' agregado, bye latches

--- tp3-bip/tp3-bip.srcs/sources_1/new/datapath.v
+++ tp3-bip/tp3-bip.srcs/sources_1/new/datapath.v
@@ -2,17 +2,18 @@
 
 module datapath
 (
-    input         i_clk,
-    input         i_reset,
-    input  [1:0]  i_sel_a,
-    input         i_sel_b,
-    input         i_write_acc,
-    input         i_operacion,
-    input  [10:0] i_operando,
-    input  [15:0] i_memory_data,
+    input  wire        i_clk,
+    input  wire        i_reset,
+    input  wire        i_valid,
+    input  wire [1:0]  i_sel_a,
+    input  wire        i_sel_b,
+    input  wire        i_write_acc,
+    input  wire        i_operacion,
+    input  wire [10:0] i_operando,
+    input  wire [15:0] i_memory_data,
     
-    output [15:0] o_memory_data,
-    output [10:0] o_data_addr
+    output wire [15:0] o_memory_data,
+    output wire [10:0] o_data_addr
 );
 
 	//multiplexores
@@ -30,32 +31,45 @@ module datapath
 	// Estension de signo
 	// repite 5 veces el MSB de i_operando (signo)
 	always@(*)begin:sig_ext
-	   signal_ext_out = {{5{i_operando[10]}},i_operando};
+	   signal_ext_out = 16'b0;
+	   
+	   if(i_valid)
+	       signal_ext_out = {{5{i_operando[10]}},i_operando};
 	end
 	
 	// Multiplexor A
     always @(*)begin:mux_a
-        case(i_sel_a)
-            MEMORIA:   mux_a_out = i_memory_data;
-            OPERANDO:  mux_a_out = signal_ext_out;
-            RESULTADO: mux_a_out = alu_out;
-            default:   mux_a_out = 15'b0; 
-        endcase
+        mux_a_out = 16'b0;
+        
+        if(i_valid)
+        begin
+            case(i_sel_a)
+                MEMORIA:   mux_a_out = i_memory_data;
+                OPERANDO:  mux_a_out = signal_ext_out;
+                RESULTADO: mux_a_out = alu_out;
+                default:   mux_a_out = 15'b0; 
+            endcase
+        end
     end
 
     // Multiplexor B
     always @(*)begin:mux_b
-        case(i_sel_b)
-            1'b0:   mux_b_out = i_memory_data;
-            1'b1:   mux_b_out = signal_ext_out;
-        endcase
+        mux_b_out = 16'b0;
+        
+        if(i_valid)
+        begin
+            case(i_sel_b)
+                1'b0:   mux_b_out = i_memory_data;
+                1'b1:   mux_b_out = signal_ext_out;
+            endcase
+        end
     end
 
     // Acumulador
-    always @(negedge i_clk)begin:accu//o posedge?
+    always @(negedge i_clk)begin:accu
         if(i_reset)
             acc <= 16'b0;
-        else 
+        else if(i_valid) 
         begin
             if(i_write_acc)
                 acc <= mux_a_out;
@@ -66,10 +80,15 @@ module datapath
 
     // Operacion
     always @(*)begin:operation
-        case(i_operacion)
-            1'b0:   alu_out = acc + mux_b_out;
-            1'b1:   alu_out = acc - mux_b_out;
-        endcase
+        alu_out = 16'b0;
+        
+        if(i_valid)
+        begin
+            case(i_operacion)
+                1'b0:   alu_out = acc + mux_b_out;
+                1'b1:   alu_out = acc - mux_b_out;
+            endcase
+        end
     end
 
     assign o_data_addr   = i_operando;
